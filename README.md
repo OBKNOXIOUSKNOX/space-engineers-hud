02bed28876e5f96248ef8488f0d645aaf30ff388
OBKNOXIOUSKNOX <animatorknox@gmail.com>
2023-08-11 14:02:42 -0400

    Revert "Updates"
    This reverts commit 99f0adf043031eac19879fb3a0cdbb2d99a73956.

---
+++ README.md
@@ -0,0 +1,3 @@
+Read me file created
+
+First step for this project is to transfer all current code on desktop into github
---
+++ src/Tester.js
@@ -0,0 +1,5 @@
+
+function Tester () {
+    return <h1>This is tester component for the deployed project</h1>
+}
+export default Tester
